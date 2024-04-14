ab53a9618fb0adcee73c4bf109deb03ddbae4133
Charles Rocheleau <charles.rocheleau@rocketmail.com>
2024-04-14 18:01:23 -0400

    Correction pour deploiement docs #5

--- trpr02-app/docs/.vitepress/config.ts
+++ trpr02-app/docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Revue code TP02",
   description: "Revue de code du TP02 de Adam Royer et Charles Rocheleau",
-  base: '/appweb-trpr02/trpr02-app/',
+  base: '/appweb-trpr02/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
